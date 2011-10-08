fdcb901431c94fced4cb6006d92ba9bd169733d5
Alexander Terentyev <gyran@bk.ru>
2011-10-09 09:22:06 +1100

    Move path to database from Config.pm to apache configuration file

--- server/SmallWorld/Config.pm
+++ server/SmallWorld/Config.pm
@@ -9,7 +9,7 @@ our @ISA    = qw( Exporter );
 our @EXPORT = qw( DB_NAME DB_LOGIN DB_PASSWORD DB_MAX_BLOB_SIZE );
 
 
-use constant DB_NAME          => "D:/work/SmallWorld/server/db/smallworld.fdb";
+use constant DB_NAME          => $ENV{DB_PATH};
 use constant DB_LOGIN         => "sysdba";
 use constant DB_PASSWORD      => "masterkey";
 use constant DB_MAX_BLOB_SIZE => 1048576;
