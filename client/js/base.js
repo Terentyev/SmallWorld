ddac107742577be22a3bc618068bcb3a5bebd13c
pnazarov <pnazarov.vl@gmail.com>
2011-11-21 12:24:24 +1100

    #12 added getMapList joinGame

--- client/js/base.js
+++ client/js/base.js
@@ -3,24 +3,28 @@ var cmdErrors = {
   'badUsername':'Bad username',
   'badPassword':'Bad password',
   'usernameTaken': 'Username already taken',
-  'badUserSid': 'You are not loged in',
-  'badUsernameOrPassword': 'badUsernameOrPassword error'
+  'badUserSid': 'You are not logged in',
+  'badUsernameOrPassword': 'Wrong username or password'
 };
 
 messages = new Array();
 maps = new Array();
 games = new Array();
-maxMessagesCount = 5;
+maxMessagesCount = 5, sentedGameId = null;
 var data = {
       playerId: null,
       username: null,
       gameId: null,
-      gameName: null,
       mapId: null,
       sid: 0
     };
 
-function sendRequest(query, callback) {
+function showError(error, container) {
+  if (container) $(container).html(error);
+  else alert(error);
+}
+
+function sendRequest(query, callback, errorContainer) {
   $.ajax({
   	type: "POST",
 	  url: "http://client.smallworld",
@@ -28,25 +32,33 @@ function sendRequest(query, callback) {
 	  timeout: 10000,
     data: {request: JSON.stringify(query), address: serverUrl},
     beforeSend: function() {
-      $.blockUI({ message: '<h3><img src="/client/pics/loading.gif" /> Loading...</h3>' });
+      $.blockUI({ message: '<h3><img src="./pics/loading.gif" /> Loading...</h3>' });
     },
 	  success: function(response)	{
-		  if (!response['result']) {
-			  alert("Unknown server response: " + JSON.stringify(response));
-			  return;
-		  }
+		  if (!response.result)
+			  showError("Unknown server response: " + JSON.stringify(response));
+		  else if (response.result == 'ok')
+		    callback(response);
+		  else
+        showError(response.result, errorContainer);
 		  //alert(JSON.stringify(response));
-      callback(response);
-       $.unblockUI();
+      $.unblockUI();
     },
     error: function(jqXHR, textStatus, errorThrown) {
       $.unblockUI();
       alert(textStatus);
-     }
+    }
   });
 }
 
 function _setCookie(key, value) {
-  for (var i in key)
-    $.cookie(key[i], value[i]);
+  for (var i in key) $.cookie(key[i], value[i]);
+}
+
+function addRow(list) {
+  var s = '<tr>';
+  for (var i in list)
+    s += $.sprintf('<td>%s</td>', list[i]);
+  s += '</tr>'
+  return s;
 }
