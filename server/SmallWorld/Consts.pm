61d6a61c71b4c82777d384416e56bcd9109c885f
Alexander Terentyev <gyran@bk.ru>
2011-12-01 20:27:57 +1100

    Fix package format

--- server/SmallWorld/Consts.pm
+++ server/SmallWorld/Consts.pm
@@ -664,6 +664,6 @@ use constant GS_BEFORE_FINISH_TURN => 'finishTurn'       ;
 use constant GS_FINISH_TURN        => 'beforeFinishTurn' ;
 use constant GS_IS_OVER            => 'gameOver'         ;
 
-__END__
-
 1;
+
+__END__
