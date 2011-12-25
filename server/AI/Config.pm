a425d5f3dc0c79e838109dc172dafe5dee45f14d
Alexander Terentyev <gyran@bk.ru>
2011-12-25 14:00:10 +1100

    #64 Add log file name and increase default timeout

--- server/AI/Config.pm
+++ server/AI/Config.pm
@@ -8,13 +8,17 @@ use utf8;
 require Exporter;
 our @ISA    = qw( Exporter );
 our @EXPORT = qw(
+    DEFAULT_REQ_LOG_SIZE
     DEFAULT_SERVER_ADDRESS
     DEFAULT_TIMEOUT
+    LOG_FILE
 );
 
 
+use constant DEFAULT_REQ_LOG_SIZE   => 20;
 use constant DEFAULT_SERVER_ADDRESS => 'server.smallworld';
-use constant DEFAULT_TIMEOUT        => 1;
+use constant DEFAULT_TIMEOUT        => 2;
+use constant LOG_FILE               => 'ai.log';
 
 1;
 
