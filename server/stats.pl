e55d3cb91c3e015e6dfb1061eedaa4cdd457c069
Alexander Terentyev <gyran@bk.ru>
2012-02-01 12:28:28 +1100

    Fix bug in regexp. Extend output statistics

--- server/stats.pl
+++ server/stats.pl
@@ -9,20 +9,22 @@ use strict;
 die 'Specify input file, please' if !@ARGV;
 
 open FILE, '<', $ARGV[0] or die "Can not open file '$ARGV[0]'";
-my ($wins, $fails, $games) = (0, 0, 0);
+my ($wins, $fails, $games, $coins1, $coins2) = (0, 0, 0, 0, 0);
 while ( 1 ) {
   my $first = <FILE>;
   my $second = <FILE>;
   last if !$first || !$second;
 
   foreach ( \$first, \$second ) {
-    if ( $$_ =~ m/"coins":(\d)/ ) {
+    if ( $$_ =~ m/"coins":(\d+)/ ) {
       $$_ = $1;
     }
     else {
       die "Can not extract coins in line '$$_'";
     }
   }
+  $coins1 += $first;
+  $coins2 += $second;
   if ( $first > $second ) {
     ++$wins;
   }
@@ -33,8 +35,13 @@ while ( 1 ) {
 }
 close FILE;
 
+$coins1 = $coins1 / $games;
+$coins2 = $coins2 / $games;
+
 print "
 Games:       $games
 First wins:  $wins
 Second wins: $fails
+First average coins:  $coins1
+Second average coins: $coins2
 ";
