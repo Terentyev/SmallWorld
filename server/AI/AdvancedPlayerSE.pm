aace9d3524730dbb5c9dac372938d93d53d8db26
pnazarov <pnazarov.vl@gmail.com>
2012-02-04 10:56:41 +1100

    #99 Changed decline condition

--- server/AI/AdvancedPlayerSE.pm
+++ server/AI/AdvancedPlayerSE.pm
@@ -34,8 +34,6 @@ sub _constructBadgesEstimates {
   my $i = 0;
   my $phase = $self->_getGamePhase($g);
   foreach ( @{ $g->{gs}->tokenBadges } ) {
-    swLog(LOG_FILE, 'no race', $_->{raceName}) unless EST_RACE->{$_->{raceName}}->[$phase];
-    swLog(LOG_FILE, 'no power', $_->{specialPowerName}) unless EST_RACE->{$_->{raceName}}->[$phase];
     push @result, {
       est => (
         (EST_RACE->{$_->{raceName}}->[$phase] + 
@@ -56,10 +54,11 @@ sub _shouldConquer {
   my $p = $g->{gs}->getPlayer;
   my $dregs = $p->declinedRace->regions;
   my $aregs = $p->activeRace->regions;
+  my $availableTokens = $p->tokens + $p->activeRace->redeployTokensBonus($p);
 
   #если раса в упадке слаба то decline, много фигурок на руках или достаточно регионов то не decline
-  return scalar(@$dregs) > 1 &&
-         ($p->tokens + $p->activeRace->redeployTokensBonus($p) > 4 || scalar(@$dregs) + scalar(@$aregs) > 9);
+  return scalar(@$dregs) < 2 && $p->declinedTokenBadgeId == -1  && $availableTokens > 4 ||
+         scalar(@$dregs) > 1 && ($availableTokens > 4 || scalar(@$dregs) + scalar(@$aregs) > 9);
 }
 
 1;
